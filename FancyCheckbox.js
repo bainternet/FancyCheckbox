02194b894caa07d74c09d09a643eb3cbd3ffa3fc
bainternet <admin@bainternet.info>
2012-12-31 02:31:28 +0200

    updated demo.html and fixed a minor but in js

--- FancyCheckbox.js
+++ FancyCheckbox.js
@@ -110,14 +110,12 @@
 		}
 	};
 	$.fn.FancyCheckbox =  function( method ) {
-    
 		if ( methods[method] ) {
 			return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
 		} else if ( typeof method === 'object' || ! method ) {
 			return methods.init.apply( this, arguments );
 		} else {
 			$.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
-		}    
-	  
+		}
 	};
 })( jQuery, window, document );
